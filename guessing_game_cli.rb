6e89f3a97a3ed8122ee8ec94faee3b96cb0e49f1
Hope Giometti <hopegiometti@gmail.com>
2019-10-03 17:44:16 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -5,7 +5,7 @@ def run_guessing_game
   puts "Guess a number between 1 and 6.\n"
   user_input = gets.chomp
   random_number = rand(1..6)
-  
+
   if user_input.to_i == random_number
     puts "You guessed the correct number!"
   elsif user_input != random_number

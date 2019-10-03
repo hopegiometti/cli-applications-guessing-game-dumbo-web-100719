8b11ce34ba5ea6389af845a6d1e84ecd04486c35
Hope Giometti <hopegiometti@gmail.com>
2019-10-03 17:40:46 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,14 +1,4 @@
-#def generate_random_number
-#  rand(1..6)
-#end
 
-#def prompt_user
-#  puts "Guess a number between 1 and 6"
-#end
-
-#def gets_user_input
-#  gets.chomp
-#end
 require 'pry'
 
 def run_guessing_game
